3b9afbe5e771bf8b534bde9e684528971519ac18
Irshd <irshad.mohd8186@gmail.com>
2023-02-27 14:34:09 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -8,15 +8,26 @@ app.use(express.json());
 app.use("/api/posts", instacloneRoutes);
 mongoose.set("strictQuery", false);
 
-mongoose.connect(
-  "mongodb+srv://root:Hyderabad@12@cluster0.dkysp1w.mongodb.net/?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("connected to DB");
-  }
-);
-// app.use(cors());
-// mongodb+srv://vulcan:12,@expressmongodb.xgwpawz.mongodb.net/instaClone?retryWrites=true&w=majority
+app.use(express.static("build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+const mongo = process.env.MONGO_URI;
+mongoose.connect(mongo);
+mongoose.connection.on("connected", () => {
+  console.log(`connected to db`);
+});
+mongoose.connection.on("error", () => {
+  console.log(`connected to db define error`);
+});
+// mongoose.connect(
+//   "mongodb+srv://Irshad:Irshad@123@cluster0.5scvlru.mongodb.net/?retryWrites=true&w=majority",
+//   () => {
+//     console.log("connected to DB");
+//   }
+// );
+// // app.use(cors());
+// // mongodb+srv://vulcan:12,@expressmongodb.xgwpawz.mongodb.net/instaClone?retryWrites=true&w=majority
 
 app.listen(4000, () => {
   console.log("listening on 4000");
